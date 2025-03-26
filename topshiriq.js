7c723b4eee5af6f99b8349d64349ee5b674bcd77
rakhmatov0128 <hayotrakhmatov1@gmail.com>
2025-03-26 16:15:23 +0500

    tugad

---
+++ topshiriq.js
@@ -0,0 +1,64 @@
+`use strict`;
+// let togriTanga = [50,25, 10, 5];
+// let qarz = 50;
+//
+//
+// for (; qarz > 0; ) {
+//     let tanga = parseInt(prompt(`Qarz: ${qarz} tiyinnTanga tashlang (5, 10 , 25 yoki 50)`));
+//
+//     if (togriTanga.includes(tanga)) {
+//         qarz -= tanga;
+//     } else {
+//         alert("Noto‘g‘ri tanga! Faqat 5, 10 yoki 25 tiyin tashlang.");
+//     }
+// }
+//
+// console.log(`Qaytim: ${Math.abs(qarz)} tiyin`);
+// ------------------------------------
+
+// const mahsulotlar = {
+//     olma:130,
+//     nok:100,
+//     banana:100,
+//     uzum:90
+// }
+//
+// let mevaNomi = prompt(`meva nomi ...`).toLowerCase();
+// for (let meva in mahsulotlar) {
+//     if (meva === mevaNomi) {
+//         alert(`caloriya ${mahsulotlar[meva]}`)
+//     }else ((`OLMA`,`NOK`,`BANANA`,`UZUM`))
+// }
+// ----------------------------------
+// let soz = prompt("soz kirit");
+// let unli = " i,o,a,e,u,I,U,O,A,E,"
+// let natija =""
+//
+// for (let i = 0; i < soz.length; i++) {
+//     if (unli.includes(soz[i])) {
+//         alert(soz[i]);
+    // }else {
+    //     natija += soz[i]
+    // }
+// }
+// alert(natija);
+
+
+
+
+
+
+
+
+
+
+// function soz(){
+//     let unli = prompt.slice(`owefhwefoihwoefhiwfjoiwhoeifhowihio`)
+//
+//     if( unli ===[`i,o,a,e,u`]){
+//         delete unli[`i`,`o`,`a`,`e`,`u`];
+//     }else (unli[`i`])
+//         alert(unli[`i`]);
+//
+// }
+// soz()
